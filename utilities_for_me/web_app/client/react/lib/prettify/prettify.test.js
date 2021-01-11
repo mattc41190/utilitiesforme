efbc3a720a8e4443310571ca32ff12897a992e53
Matt Cale <mattc41190@gmail.com>
2021-01-10 22:26:20 -0600

    feat: adding jest to test javadcript assets

---
+++ utilities_for_me/web_app/client/react/lib/prettify/prettify.test.js
@@ -0,0 +1,35 @@
+const prettify = require('./index')
+
+const expectedJSON = 
+`{
+  "key": "val"
+}`
+
+const expectedHTML = 
+`<p>hello</p>
+<p>world</p>`
+
+const expectedJS = 
+`let fn = () => {
+    fakeFunc()
+}`
+
+const expectedCSS = `p {
+    color: red;
+}`
+
+test('prettify JSON', () => {
+    expect(prettify.prettify('{"key": "val"}', 'json')).toEqual(expectedJSON)
+})
+
+test('prettify HTML', () => {
+    expect(prettify.prettify('<p>hello</p><p>world</p>', 'html')).toEqual(expectedHTML)
+})
+
+test('prettify JS', () => {
+    expect(prettify.prettify('let fn = () => { fakeFunc() }', 'js')).toEqual(expectedJS)
+})
+
+test('prettify CSS', () => {
+    expect(prettify.prettify('p {color: red;}', 'css')).toEqual(expectedCSS)
+})
