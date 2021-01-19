b9271468552b4e1d97882f25e09f1ed2ef35eeca
Matt Cale <mattc41190@gmail.com>
2021-01-19 12:17:10 -0600

    fix: calc percent example scripts

--- examples/calculate_percent/calculate_percent_examples.sh
+++ examples/calculate_percent/calculate_percent_examples.sh
@@ -1,11 +1,11 @@
 echo -----
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data '{ "percent":"50", "of":"100" }' \
-  http://localhost:5050/api/v1/calculate-percent/calculate-percent-of
+  --data '{ "percent":"50", "of":"100", "formula": "percent_of" }' \
+  http://localhost:5050/api/v1/calculate-percent
 
 echo -----
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data '{ "num":"50", "of":"100" }' \
-  http://localhost:5050/api/v1/calculate-percent/calculate-num-is-what-percent-of
+  --data '{ "num":"50", "of":"100", "formula": "num_is_what_percent_of" }' \
+  http://localhost:5050/api/v1/calculate-percent
