af62b5e9066a785dcd33517b2196f85a5dc217c3
Matt Cale <mattc41190@gmail.com>
2021-09-17 10:40:16 -0500

    feat: improvments to count util

--- utilities_for_me/web_app/client/react/Count.jsx
+++ utilities_for_me/web_app/client/react/Count.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect } from 'react'
+import React, { useReducer, useEffect, useState } from 'react'
 import Button from './common/Button'
 import COLORS from './lib/colors'
 
@@ -15,13 +15,18 @@ function useKeyPress (targetKey, cb) {
   })
 }
 
+const NOOP = () => {}
+
 const ADD = 'counter/add'
 const REMOVE = 'counter/remove'
 const INCREMENT = 'counter/increment'
 const DECREMENT = 'counter/decrement'
 const SET_FOCUS = 'counter/setFocus'
+const SET_NAME = 'counter/setName'
+const SET_HOT_KEYS = 'counter/setHotKeys'
 
 const INITIAL_STATE = {
+  hotKeysEnabled: true,
   counters: [
     {
       id: 1,
@@ -30,6 +35,34 @@ const INITIAL_STATE = {
       hasFocus: true
     }
   ]
+}
+
+const setNameAction = (name, id) => {
+  return { type: SET_NAME, payload: { counterId: id, name: name } }
+}
+
+const setHotKeysAction = () => {
+  return { type: SET_HOT_KEYS, payload: {} }
+}
+
+const addCounterAction = () => {
+  return { type: ADD, payload: {} }
+}
+
+const removeCounterAction = (id) => {
+  return { type: REMOVE, payload: { counterId: id } }
+}
+
+const incrementCounterAction = (id, amount) => {
+  return { type: INCREMENT, payload: { counterId: id, amount: amount } }
+}
+
+const decrementCounterAction = (id, amount) => {
+  return { type: DECREMENT, payload: { counterId: id, amount: amount } }
+}
+
+const setFocusAction = (id) => {
+  return { type: SET_FOCUS, payload: { counterId: id } }
 }
 
 const findCounterById = (counters, id) => {
@@ -91,6 +124,9 @@ const decrementReducer = (state, payload) => {
 }
 
 const addReducer = (state, payload) => {
+  if (state.counters.length > 256) {
+    return state
+  }
   const newId = getNextId(state.counters)
   const newCounter = {
     id: newId,
@@ -112,11 +148,13 @@ const addReducer = (state, payload) => {
 
 const removeReducer = (state, payload) => {
   if (state.counters.length < 2) {
-    return state
+    const newCounter = { ...state.counters[0] }
+    newCounter.hasFocus = true
+    return { ...state, counters: [newCounter] }
   }
 
   const newCounters = state.counters.filter(counter => counter.id !== payload.counterId)
-  const counterWithFocus = state.counters.find(counter => counter.hasFocus)
+  const counterWithFocus = newCounters.find(counter => counter.hasFocus)
   if (!counterWithFocus) {
     newCounters[0].hasFocus = true
   }
@@ -125,7 +163,7 @@ const removeReducer = (state, payload) => {
   return { ...state, counters: newCounters }
 }
 
-const setFocus = (state, payload) => {
+const setFocusReducer = (state, payload) => {
   const newCounters = state.counters.map(counter => {
     if (payload.counterId === counter.id) {
       return {
@@ -142,22 +180,59 @@ const setFocus = (state, payload) => {
   return { ...state, counters: newCounters }
 }
 
+const setNameReducer = (state, payload) => { 
+  const newCounters = state.counters.map(counter => {
+    if (payload.counterId === counter.id) {
+      return {
+        ...counter,
+        name: payload.counterId
+      }
+    }
+    return {
+      ...counter,
+    }
+  }) // Mutate
+  sortCounters(newCounters)
+  return { ...state, counters: newCounters }
+}
+
+const setHotKeysReducer = (state, payload) => { 
+  return { ...state, hotKeysEnabled: !state.hotKeysEnabled }
+}
+
+const hackyLoggerMiddleware = (state) => {
+  console.groupCollapsed('%cNew State', 'color: green')
+  console.log(state)
+  console.groupEnd()
+  console.groupEnd()
+  return state
+}
+
 const counterReducer = (state, action) => {
-  console.log(`state: ${JSON.stringify(state)}`)
-  console.log(`action: ${JSON.stringify(action)}`)
+  console.groupCollapsed(`${action.type} @ ${Date.now()}`)
+  console.groupCollapsed('%cOld State', 'color: blue')
+  console.log(state)
+  console.groupEnd()
+  console.groupCollapsed(`%cAction (${action.type})`, 'color: red')
+  console.log(action)
+  console.groupEnd()
   switch (action.type) {
     case INCREMENT:
-      return incrementReducer(state, action.payload)
+      return hackyLoggerMiddleware(incrementReducer(state, action.payload))
     case DECREMENT:
-      return decrementReducer(state, action.payload)
+      return hackyLoggerMiddleware(decrementReducer(state, action.payload))
     case ADD:
-      return addReducer(state, action.payload)
+      return hackyLoggerMiddleware(addReducer(state, action.payload))
     case REMOVE:
-      return removeReducer(state, action.payload)
+      return hackyLoggerMiddleware(removeReducer(state, action.payload))
     case SET_FOCUS:
-      return setFocus(state, action.payload)
+      return hackyLoggerMiddleware(setFocusReducer(state, action.payload))
+    case SET_NAME:
+      return hackyLoggerMiddleware(setNameReducer(state, action.payload))
+    case SET_HOT_KEYS:
+      return hackyLoggerMiddleware(setHotKeysReducer(state, action.payload))
     default:
-      return state
+      return hackyLoggerMiddleware(state)
   }
 }
 
@@ -165,7 +240,7 @@ const CounterHeader = () => {
   return (
     <section className='p-2'>
       <h1 className='text-5xl font-light mb-3'>Count</h1>
-      <p>The <i>Count</i> utility will help you keep track of the count of one thing or many things.</p>
+      <p>The <i>Count</i> utility will help you keep track of the count of one thing or many things. Use hot keys like: <code>a</code> to add a new counter, <code>n</code> to switch focus, and arrow keys to increment by 1 or 10. </p>
     </section>
   )
 }
@@ -173,7 +248,7 @@ const CounterHeader = () => {
 const RemoveCounter = ({ id, dispatch }) => {
   return (
     <Button
-      handleClick={() => dispatch({ type: REMOVE, payload: { counterId: id } })}
+      handleClick={() => dispatch(removeCounterAction(id))}
       value='x' label='X'
       color={COLORS.red}
       hoverColor={COLORS.redHover}
@@ -181,22 +256,71 @@ const RemoveCounter = ({ id, dispatch }) => {
   )
 }
 
-const CounterBody = ({ id, name, count, dispatch, hasFocus, isLast }) => {
+const CounterBody = ({ id, name, count, dispatch, hasFocus, isLast, hotKeysEnabled }) => {
+  const textClasses = 'text-theme-input-primary placeholder-theme-input-primary'
+  const borderClasses = 'rounded-md border-2 border-theme-input-primary-complement focus:border-theme-emphasis-fill focus:outline-none focus:ring-1 focus:ring-skin-emphasis-fill'
+
+  const [nameInput, setNameInput] = useState(false)
+  const [localName, setLocalName] = useState(name) 
+  useKeyPress('Enter', () => save())
+
+  const handleTitleClick = (e) => {
+    activateInput()
+  }
+
+  const activateInput = () => {
+    if (!nameInput) {
+      setNameInput(true) 
+      dispatch(setHotKeysAction())
+    }
+  }
+
+  const saveHandler = (e) => {
+    save()
+  }
+
+  const save = () => {
+    if (nameInput) {
+      setNameInput(false) 
+      dispatch(setNameAction(localName, id))
+      dispatch(setHotKeysAction())
+    }
+  }
+
   return (
     <div
-      className='flex flex-col bg-theme-comp-primary-fill rounded-xl p-6 md:p-2 shadow-inner justify-center text-center mt-4 mb-3'
+      onClick={() => dispatch(setFocusAction(id))}
+      className='flex flex-col bg-theme-comp-primary-fill rounded-default p-6 md:p-2 shadow-inner justify-center text-center mt-4 mb-3'
     >
-      <div className='text-4xl mb-2'>{name}{hasFocus ? ' ⬅️' : null}</div>
-      <div className='text-4xl mb-5'>{count}</div>
+      <div 
+        onClick={handleTitleClick} 
+        className='text-4xl mb-2 text-theme-comp-primary'>
+          <span> 
+            {nameInput 
+              ? <input
+                  autoFocus 
+                  className={`w-48 p-2 m-3 border-2 rounded bg-theme-input-primary-fill ${textClasses} ${borderClasses}`}
+                  type="text" 
+                  value={localName}
+                  onChange={(e) => { setLocalName(e.target.value) }}
+                >
+                </input> 
+              : localName
+            } 
+          </span>
+          {nameInput ? <button onClick={saveHandler}>✅</button> : null}
+          {hasFocus ? ' ⬅️' : null}
+      </div>
+      <div className='text-4xl mb-5 text-theme-comp-primary'>{count}</div>
       <div>
         <Button
-          handleClick={() => dispatch({ type: INCREMENT, payload: { counterId: id, amount: 1 } })}
+          handleClick={() => dispatch(incrementCounterAction(id, 1))}
           value='inc' label='Increment'
           color={COLORS.green}
           hoverColor={COLORS.greenHover}
         />
         <Button
-          handleClick={() => dispatch({ type: DECREMENT, payload: { counterId: id, amount: 1 } })}
+          handleClick={() => dispatch(decrementCounterAction(id, 1))}
           value='dec' label='Decrement'
           color={COLORS.yellow}
           hoverColor={COLORS.yellowHover}
@@ -207,16 +331,12 @@ const CounterBody = ({ id, name, count, dispatch, hasFocus, isLast }) => {
             : <RemoveCounter id={id} dispatch={dispatch} />
         }
       </div>
-
     </div>
   )
 }
 
 const findNextCounterId = (counters) => {
   const currentFocusCounterIndex = counters.findIndex(c => c.hasFocus)
-  console.log(`currentFocusCounterIndex: ${currentFocusCounterIndex}`)
-  console.log(`counters.length - 1: ${counters.length - 1}`)
-
   if (counters.length - 1 <= currentFocusCounterIndex) {
     return counters[0].id
   }
@@ -225,29 +345,33 @@ const findNextCounterId = (counters) => {
 
 function Count () {
   const [state, dispatch] = useReducer(counterReducer, INITIAL_STATE)
+
   const currCounter = state.counters.find(c => c.hasFocus)
   const currCounterId = currCounter ? currCounter.id : -1
   const nextCounterId = findNextCounterId(state.counters)
-  useKeyPress('Tab', () => dispatch({ type: SET_FOCUS, payload: { counterId: nextCounterId } }))
-  useKeyPress(' ', () => dispatch({ type: ADD, payload: {} }))
-  useKeyPress('Backspace', () => dispatch({ type: REMOVE, payload: { counterId: currCounterId } }))
-  useKeyPress('ArrowRight', () => dispatch({ type: INCREMENT, payload: { counterId: currCounterId, amount: 1 } }))
-  useKeyPress('ArrowLeft', () => dispatch({ type: DECREMENT, payload: { counterId: currCounterId, amount: 1 } }))
-  useKeyPress('ArrowUp', () => dispatch({ type: INCREMENT, payload: { counterId: currCounterId, amount: 10 } }))
-  useKeyPress('ArrowDown', () => dispatch({ type: DECREMENT, payload: { counterId: currCounterId, amount: 10 } }))
+  const hotKeysEnabled = state.hotKeysEnabled
+  const noop = () => {}
+
+  useKeyPress('n', hotKeysEnabled ? () => dispatch(setFocusAction(nextCounterId)) : NOOP)
+  useKeyPress('a', hotKeysEnabled ? () => dispatch(addCounterAction()) : NOOP)
+  useKeyPress('r', hotKeysEnabled ? () => dispatch(removeCounterAction(currCounterId)) : NOOP)
+  useKeyPress('ArrowRight', hotKeysEnabled ? () => dispatch(incrementCounterAction(currCounterId, 1)) : NOOP )
+  useKeyPress('ArrowLeft', hotKeysEnabled ? () => dispatch(decrementCounterAction(currCounterId, 1)): NOOP)
+  useKeyPress('ArrowUp', hotKeysEnabled ? () => dispatch(incrementCounterAction(currCounterId, 10)) : NOOP)
+  useKeyPress('ArrowDown', hotKeysEnabled ? () => dispatch(decrementCounterAction(currCounterId, 10)): NOOP)
 
   return (
-    <div className='mt-6 text-skin-primary'>
+    <div className='mt-6 mx-3 text-skin-primary'>
       <CounterHeader />
       <hr />
       <div className='mt-3'>
         <Button
-          handleClick={() => dispatch({ type: ADD, payload: {} })}
+          handleClick={() => dispatch(addCounterAction())}
           value='add' label='Add New'
           color={COLORS.green}
           hoverColor={COLORS.greenHover}
         />
-        <div className='grid md:grid-cols-3 gap-3'>
+        <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-3'>
           {state.counters.map(counter => {
             return (
               <CounterBody
